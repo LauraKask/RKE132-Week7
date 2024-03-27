ae94403e49c81fd4fd83274179cea42fe7a98545
Laura <laura.kask@tktk.ee>
2024-03-27 14:20:56 +0200

    add week7 code

---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter the operation (+/-):");
+char userOperator = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a:");
+int firstNum = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Enter b:");
+int secundNum = Int32.Parse(Console.ReadLine());
+
+switch(userOperator)
+{
+    case '+':
+        Addition(firstNum, secundNum);
+        break;
+    case '-':
+        Subtraction(firstNum, secundNum);
+        break;
+    default:
+        Console.WriteLine("invalid operator");
+        break;
+}
+static void Addition(int a, int b)
+{
+    Console.WriteLine($"{a} + {b} = {a + b}");
+}
+
+static void Subtraction (int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+}
---
+++ Funcsions/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/ouy):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+ static void PrintHello() // fuctsion ehk meetod
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later!");
+}
